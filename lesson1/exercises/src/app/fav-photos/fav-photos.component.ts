cb7d3dcd9dbca3d5564e21b1819015718cb3c3b9
Molly Osburg <mollyosburg@Mollys-MacBook-Air.local>
2021-04-23 11:40:03 -0500

    added images and tags according to the directions

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Excellent Photos';
+  image1 = 'https://image.shutterstock.com/image-photo/costa-mesa-california-september-23-600w-720648007.jpg';
+  image2 = 'https://image.shutterstock.com/image-vector/alien-head-watching-3d-movie-600w-1669619188.jpg';
+  image3 = 'https://image.shutterstock.com/image-photo/movie-clapperboard-halloween-decoration-horror-600w-1814840768.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Lost Boys', 'Aliens', 'The Blues Brothers', 'The Princess Bride'];
 
    constructor() { }
 
